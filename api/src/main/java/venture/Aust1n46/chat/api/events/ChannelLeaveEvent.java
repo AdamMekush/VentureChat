ed7b5396f36af327923f37a97a795df072627cfb
Adam Mekush <60749996+AdamMekush@users.noreply.github.com>
2024-09-07 02:14:57 +0200

    Imported some API events from branch api-implementation

---
+++ api/src/main/java/venture/Aust1n46/chat/api/events/ChannelLeaveEvent.java
@@ -0,0 +1,48 @@
+package venture.Aust1n46.chat.api.events;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.Cancellable;
+import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
+import venture.Aust1n46.chat.model.IChatChannel;
+
+public class ChannelLeaveEvent extends Event implements Cancellable {
+    private static final HandlerList handlers = new HandlerList();
+    private boolean cancelled;
+    private Player player;
+    private IChatChannel channel;
+
+    public ChannelLeaveEvent(Player player, IChatChannel channel) {
+        this.player = player;
+        this.channel = channel;
+
+        this.cancelled = false;
+    }
+
+    @Override
+    public HandlerList getHandlers() {
+        return handlers;
+    }
+
+    public static HandlerList getHandlerList() {
+        return handlers;
+    }
+
+    @Override
+    public boolean isCancelled() {
+        return this.cancelled;
+    }
+
+    @Override
+    public void setCancelled(boolean cancel) {
+        this.cancelled = cancel;
+    }
+
+    public Player getPlayer() {
+        return this.player;
+    }
+
+    public IChatChannel getChannel() {
+        return this.channel;
+    }
+}
---
+++ api/src/main/java/venture/Aust1n46/chat/api/events/KickChannelPlayerEvent.java
@@ -0,0 +1,54 @@
+package venture.Aust1n46.chat.api.events;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.Cancellable;
+import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
+import venture.Aust1n46.chat.model.IChatChannel;
+
+import java.util.Set;
+
+public class KickChannelPlayerEvent extends Event implements Cancellable {
+    private static final HandlerList handlers = new HandlerList();
+    private boolean cancelled;
+    private Player victim;
+    private Player operator;
+    private Set<IChatChannel> channels;
+
+    public KickChannelPlayerEvent(Player victim, Player operator, Set<IChatChannel> channels) {
+        this.victim = victim;
+        this.operator = operator;
+        this.channels = channels;
+    }
+
+    @Override
+    public HandlerList getHandlers() {
+        return handlers;
+    }
+
+    public static HandlerList getHandlerList() {
+        return handlers;
+    }
+
+    @Override
+    public boolean isCancelled() {
+        return this.cancelled;
+    }
+
+    @Override
+    public void setCancelled(boolean cancel) {
+        this.cancelled = cancel;
+    }
+
+    public Player getVictim() {
+        return this.victim;
+    }
+
+    public Player getOperator() {
+        return this.operator;
+    }
+
+    public Set<IChatChannel> getChannels() {
+        return this.channels;
+    }
+}
