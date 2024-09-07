bd4dd06548c96491fb12b129f0e6dd9580ac0869
Adam Mekush <60749996+AdamMekush@users.noreply.github.com>
2024-09-07 05:16:47 +0200

    Refactoring

--- api/src/main/java/venture/Aust1n46/chat/api/events/KickChannelPlayerEvent.java
+++ api/src/main/java/venture/Aust1n46/chat/api/events/KickChannelPlayerEvent.java
@@ -9,7 +9,6 @@ import venture.Aust1n46.chat.model.IVentureChatPlayer;
 
 import java.util.Set;
 
-// TODO
 public class KickChannelPlayerEvent extends Event implements Cancellable {
     private static final HandlerList handlers = new HandlerList();
     private boolean cancelled;
--- api/src/main/java/venture/Aust1n46/chat/api/events/MutePlayerEvent.java
+++ api/src/main/java/venture/Aust1n46/chat/api/events/MutePlayerEvent.java
@@ -1,7 +1,6 @@
 package venture.Aust1n46.chat.api.events;
 
 import lombok.Getter;
-import org.bukkit.entity.Player;
 import org.bukkit.event.Cancellable;
 import org.bukkit.event.Event;
 import org.bukkit.event.HandlerList;
